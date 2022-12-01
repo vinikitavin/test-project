08cf99c12383b01cdbf77c4d43640fc05cc8dcdb
vinikitavin <vinikitavin@mail.ru>
2022-12-01 08:53:45 +0600

    default project

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="app">
+  </div>
+</template>
+
+<script>
+export default {
+  components: {}
+}
+</script>
+
+<style lang="scss">
+  @import "~materialize-css/dist/css/materialize.min.css";
+</style>
